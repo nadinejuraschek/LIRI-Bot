5c46aa1df22cda30d70e505561e3615556a8179e
nadinejuraschek <nadinejwebdev@gmail.com>
2022-07-30 13:53:54 +0200

    New: colored console logs

--- operations/runLiri.js
+++ operations/runLiri.js
@@ -1,18 +1,22 @@
 import { concertThis, movieThis, spotifyThisSong } from './index.js';
 
+const red = "\x1b[31m";
+const yellow = "\x1b[33m";
+const resetBg = '\x1b[0m';
+
 export const runLIRI = (command, searchTerm) => {
   switch(command) {
     case "concert-this":
-      console.log("Your command is: " + command);
+      console.log(yellow, "Your command is: " + command, resetBg);
       return concertThis(searchTerm);
     case "movie-this":
-      console.log("Your command is: " + command);
+      console.log(yellow, "Your command is: " + command, resetBg);
       return movieThis(searchTerm);
     case "spotify-this":
-      console.log("Your command is: " + command);
+      console.log(yellow, "Your command is: " + command, resetBg);
       return spotifyThisSong(searchTerm);
     default:
-      console.log('Please use a valid command.');
+      console.log(red, 'Please use a valid command.');
       return;
   }
 };
